cdaaeb7b6bdc0cc373c5c31ceee279aaa3a426a9
baker.d.andrew@gmail.com <baker.d.andrew@gmail.com>
2020-08-20 18:27:23 -0700

    added unpacking assignment

--- main.py
+++ main.py
@@ -4,4 +4,15 @@ import random
 
 print("Welcome to my text-based DnD adventure" + "\n")
 
-protagonist_name = input("what is you character's name? : ")
+
+with open("Eldar_Dracarys.txt", "r") as character_sheet:
+    data = character_sheet.readlines()
+
+name, Strength, S_modifier,\
+Dexterity, D_modifier, Constitution, C_modifier, Intelligence,\
+I_modifier, Wisdom, W_modifier, Charisma, CH_modifier, Hit_points,\
+Current_hit_point, potential_attack_damage\
+= [d.split(":")[1].split("/n")[0] for d in data]
+
+print("Your character that you have loaded in" + "\n")
+print(name)
