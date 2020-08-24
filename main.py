dddffa9e649bc5e30dc091328fa3540542ebc816
baker.d.andrew@gmail.com <baker.d.andrew@gmail.com>
2020-08-24 10:57:55 -0700

    added skeleton encounter with class and function. needs more work, but is currently working

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ import random
 import sys
 
 
-#def is_enemy_down():
+def skeleton_damage(skeleton_hitpoints):
+    player_damage = random.randint(1, int(potential_attack_damage))
+    print("You deal " + str(player_damage) + " damage towards the skeleton")
+    skeleton_hitpoints = skeleton_hitpoints - player_damage
+    if skeleton_hitpoints <= 0:
+        print("you killed the skeleton! he returns to the under.")
+    else:
+        print("The skeleton staggers, but is 'alive'.")
+    return skeleton_hitpoints
 
 
 # main
@@ -14,7 +22,7 @@ with open("Eldar_Dracarys.txt", "r") as character_sheet:
     data = character_sheet.readlines()
 
 # assignment unpacking
-#for getting the player sheet into the game
+# for getting the player sheet into the game
 
 name, Strength, S_modifier,\
 Dexterity, D_modifier, Constitution, C_modifier, Intelligence,\
@@ -30,10 +38,18 @@ with open("Skeleton.txt", "r") as skeleton_sheet:
 skel_attack, skel_hitpoints, skel_current_hitpoints = [d.split(":")[1].split("/n")[0] for d in skel_data]
 
 
+class Skeletons:
+    def __init__ (self, skel_attack, skel_hitpoints, skel_current_hitpoints, distance):
+        self.skel_attack = int(skel_attack)
+        self.skel_hitpoints = int(skel_hitpoints)
+        self.skel_current_hitpoints = int(skel_current_hitpoints)
+        self.skel_distance = int(distance)
+
+
 print("Your character that you have loaded in" + "\n")
 print(name)
 
-game_action = input("you approach a cave with a wide mouth. It is very mysterious and you feel danger emanating " \
+game_action = input("you approach a cave with a wide mouth. It is very mysterious and you feel danger emanating "
                     "from it. do you enter? (y) or (n) : ")
 
 print("")
@@ -47,14 +63,18 @@ print("you enter the cave and it's dark with few torches lighting your way. you'
       "artificial. The cave is still rock and has random bumps to it, but the ground is relatively level and \n"\
       "easy to walk across. It is also follows an slight incline down into the ground." + "\n")
 
-print("you hear a shuffling slowly, but steadily getting louder. after walking towards it, you see a skeleton with a "\
+print("you hear a shuffling slowly, but steadily getting louder. after walking towards it, you see a skeleton with a "
       "sword! It's starts walking towards you and it looks like it's going to attack!")
 
-player_attack = input("do you use your bow or sword? : ")
 
-if "bow" in player_attack:
-    print("you pull out your bow with a silent confidence that speaks to your skill, you take an arrow \n"\
-          "and pull it against the string. You let it fly")
-    player_damage = random.randint(1, 7)
-    print("You deal " + player_damage + " towards the skeleton")
+first_skel = Skeletons(skel_attack, skel_hitpoints, skel_current_hitpoints, 1)
 
+while first_skel.skel_current_hitpoints > 0:
+    player_attack = input("do you use your bow or sword? : ")
+    if "bow" in player_attack:
+        print("\nyou pull out your bow with a silent confidence that speaks to your skill, you take an arrow \n"
+              "and pull it against the string. You let it fly")
+        first_skel.skel_current_hitpoints = skeleton_damage(first_skel.skel_current_hitpoints)
+    else:
+        print("You take out your sword with fire lighting inside you. You charge straight to your foe and strike!")
+        first_skel.skel_current_hitpoints = skeleton_damage(first_skel.skel_current_hitpoints)
